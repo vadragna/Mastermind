ce8bc051686b786785f4f74d8c7f054642966fa3
Valerio Adragna <valerioadragna89@hotmail.it>
2020-05-16 20:15:11 +0200

    working on checking logic

--- script.js
+++ script.js
@@ -1,29 +1,87 @@
-let target = 9;
-let colors = [
-  "blue",
-  "aqua",
-  "hotpink",
-  "lime",
-  "yellow",
-  "indigo",
-  "maroon",
-  "black",
-];
+(function () {
+  let target = 9;
+  let colors = [
+    "blue",
+    "aqua",
+    "hotpink",
+    "lime",
+    "yellow",
+    "indigo",
+    "red",
+    "black",
+  ];
 
-$(".column").on("click", function (e) {
-  var col = $(e.currentTarget);
-  console.log("col", col);
-  var targetSlot = col.children();
-  console.log("targetSlot", targetSlot.eq(target));
-  for (let i = 0; i < colors.length; i++) {
-    if (targetSlot.eq(target).hasClass(colors[i])) {
-      console.log("true");
-      targetSlot.eq(target).removeClass(colors[i]);
-      targetSlot.eq(target).addClass(colors[i + 1]);
-      return;
+  $("#checkButton").on("click", function (e) {
+    var sameColor = 0;
+    var samePosition = 0;
+    var dontCheck = [];
+    let rowToCheck = $(".row" + target);
+    let keyRow = $(".key");
+    let positionsToCheck = [0, 1, 2, 3];
+    let KeyToCheck = [0, 1, 2, 3];
+
+    for (var i = 0; i <= 3; i++) {
+      console.log("positionsToCheck", positionsToCheck);
+      for (var j = 0; j <= 3; j++) {
+        if (positionsToCheck.length + i < 4) {
+          console.log("continue", positionsToCheck.length, i);
+          continue;
+        }
+        if (
+          rowToCheck.eq(i).prop("classList")[2] ===
+          keyRow.eq(j).prop("classList")[3]
+        ) {
+          console.log(
+            rowToCheck.eq(i).prop("classList")[2],
+            keyRow.eq(j).prop("classList")[3],
+            i,
+            j
+          );
+          if (i === j) {
+            samePosition++;
+            dontCheck.push(j);
+            positionsToCheck.splice(i, 1);
+            KeyToCheck.splice(j, 1);
+            i++;
+          } else {
+            sameColor++;
+            dontCheck.push(j);
+            positionsToCheck.splice(i, 1);
+            KeyToCheck.splice(j, 1);
+            i++;
+          }
+        }
+      }
     }
+
+    console.log(
+      "sameColor",
+      sameColor,
+      "samePosition",
+      samePosition,
+      "dontCheck",
+      dontCheck
+    );
+    target--;
+  });
+
+  for (let i = 0; i <= 3; i++) {
+    let random = Math.floor(Math.random() * 8);
+    let keyBox = $(".key" + i);
+    keyBox.addClass(colors[random]);
   }
-  if (!targetSlot.eq(target).hasClass(colors[0])) {
+
+  $(".column").on("click", function (e) {
+    var col = $(e.currentTarget);
+    var targetSlot = col.children();
+    for (let i = 0; i < colors.length; i++) {
+      if (targetSlot.eq(target).hasClass(colors[i])) {
+        targetSlot.eq(target).removeClass(colors[i]);
+        targetSlot.eq(target).addClass(colors[i + 1]);
+        return;
+      }
+    }
+
     targetSlot.eq(target).addClass(colors[0]);
-  }
-});
+  });
+})();
